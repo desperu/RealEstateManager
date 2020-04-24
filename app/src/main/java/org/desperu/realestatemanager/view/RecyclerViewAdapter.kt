64adf463d77e165f9bda80157cb7577d43d1dc7b
desperu <motais.esteban@laposte.net>
2020-04-24 17:01:50 +0200

    Previous commit error correction, adapter commit here : *Add and comment recycler management list in adapter.*

--- app/src/main/java/org/desperu/realestatemanager/view/RecyclerViewAdapter.kt
+++ app/src/main/java/org/desperu/realestatemanager/view/RecyclerViewAdapter.kt
@@ -17,47 +17,80 @@ class RecyclerViewAdapter(@LayoutRes private val layoutId: Int): RecyclerView.Ad
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(list[position])
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(list[position])
+
 
     override fun getItemViewType(position: Int) = layoutId
 
-    override fun getItemCount(): Int = if(::list.isInitialized) list.size else 0
+    override fun getItemCount(): Int = if (::list.isInitialized) list.size else 0
 
-    fun updateList(list: MutableList<Any>) {
+    /**
+     * Get list if is initialized, else empty list.
+     */
+    internal fun getList() = if (::list.isInitialized) list else mutableListOf()
+
+    /**
+     * Update all item list.
+     * @param list the list to set.
+     */
+    internal fun updateList(list: MutableList<Any>) {
         this.list = list
         notifyDataSetChanged()
     }
 
-    fun updateItem(position: Int, any: Any) {
-        list.removeAt(position)
-        list.add(position, any)
-        notifyItemChanged(position)
-    }
+    /**
+     * Get item for given position
+     * @param position the item position to get.
+     */
+    internal fun getItem(position: Int): Any = list[position]
 
-    fun removeItem(position: Int) {
-        list.removeAt(position)
-        notifyItemRemoved(position)
-        notifyItemRangeChanged(position, list.size)
-    }
-
-    fun addItem(position: Int, any: Any) {
+    /**
+     * Add an item in list.
+     * @param position the position to add item.
+     * @param any the item to add in list.
+     */
+    internal fun addItem(position: Int, any: Any) {
         list.add(position, any)
         // notify item added by position
         notifyItemInserted(position)
     }
 
-    fun moveItem(fromPosition: Int, toPosition: Int) {
+    /**
+     * Update item in list.
+     * @param position the item position updated.
+     * @param any the new item.
+     */
+    internal fun updateItem(position: Int, any: Any) {
+        list.removeAt(position)
+        list.add(position, any)
+        notifyItemChanged(position)
+    }
+
+    /**
+     * Move an item to new position in list.
+     * @param fromPosition the actual item position move from.
+     * @param toPosition the new item position move to.
+     */
+    internal fun moveItem(fromPosition: Int, toPosition: Int) {
         val movedItem = list[fromPosition]
         list.removeAt(fromPosition)
         list.add(toPosition, movedItem)
         notifyItemMoved(fromPosition, toPosition)
     }
 
+    /**
+     * Remove an item in list.
+     * @param position the item position in list.
+     */
+    internal fun removeItem(position: Int) {
+        list.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, list.size)
+    }
+
     class ViewHolder(private val binding: ViewDataBinding): RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(any: Any?) {
+        internal fun bind(any: Any?) {
             binding.setVariable(org.desperu.realestatemanager.BR.viewModel, any)
             binding.executePendingBindings()
         }
